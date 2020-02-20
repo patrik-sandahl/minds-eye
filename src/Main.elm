e12399fe0ee0db7950a807e243fbda5a3457d27e
Patrik Sandahl <patrik.sandahl@gmail.com>
2020-02-20 21:18:41 +0100

    Simple rendering on a full screen quad.

--- src/Main.elm
+++ src/Main.elm
@@ -5,8 +5,10 @@ import Browser.Dom as Dom
 import Browser.Events as BrowserEvents
 import Html exposing (Html)
 import Html.Attributes as HtmlAttributes
+import Math.Vector2 as V2 exposing (Vec2)
+import Math.Vector3 as V3 exposing (Vec3)
 import Task
-import WebGL
+import WebGL exposing (Mesh, Shader)
 
 
 type alias Model =
@@ -49,13 +51,20 @@ fetchViewportSize =
 view : Model -> Html Msg
 view model =
     WebGL.toHtmlWith
-        [ WebGL.clearColor 1.0 0.0 0.0 1.0
+        [ WebGL.antialias
         ]
         [ floor model.viewportWidth |> HtmlAttributes.width
         , floor model.viewportHeight |> HtmlAttributes.height
         , HtmlAttributes.style "display" "block"
         ]
-        []
+        [ WebGL.entity
+            quadVertexShader
+            playgroundFragmentShader
+            quadMesh
+            { resolution = V2.vec2 model.viewportWidth model.viewportHeight
+            , playTime = model.playTime
+            }
+        ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -78,3 +87,61 @@ subscriptions _ =
         [ BrowserEvents.onResize (\w h -> ResizeViewport (toFloat w) (toFloat h))
         , BrowserEvents.onAnimationFrameDelta AnimateFrame
         ]
+
+
+type alias Vertex =
+    { position : Vec3
+    }
+
+
+type alias Uniforms =
+    { resolution : Vec2
+    , playTime : Float
+    }
+
+
+quadMesh : Mesh Vertex
+quadMesh =
+    WebGL.triangleStrip
+        [ Vertex (V3.vec3 -1.0 1.0 0.0)
+        , Vertex (V3.vec3 -1.0 -1.0 0.0)
+        , Vertex (V3.vec3 1.0 1.0 0.0)
+        , Vertex (V3.vec3 1.0 -1.0 0.0)
+        ]
+
+
+quadVertexShader : Shader Vertex Uniforms {}
+quadVertexShader =
+    [glsl|
+
+attribute vec3 position;
+
+void main()
+{
+    gl_Position = vec4(position, 1.0);
+}
+
+    |]
+
+
+playgroundFragmentShader : Shader {} Uniforms {}
+playgroundFragmentShader =
+    [glsl|
+
+precision highp float;
+
+uniform vec2 resolution;
+uniform float playTime;
+
+vec2 normalizedUV()
+{
+    return (gl_FragCoord.xy - 0.5 * resolution) / min(resolution.x, resolution.y);
+}
+
+void main()
+{
+    vec2 uv = fract(normalizedUV() * 10.0);
+    gl_FragColor = vec4(uv.x, uv.y, 0.0, 1.0);
+}
+
+    |]
