a80ee88fcaaf7226568099d00d59e9fdcdc82807
Patrik Sandahl <patrik.sandahl@gmail.com>
2020-03-05 20:08:54 +0100

    Minor stuff

--- src/Navigator.elm
+++ src/Navigator.elm
@@ -4,22 +4,38 @@ module Navigator exposing
     , init
     )
 
+import Camera exposing (Camera)
 import Sphere exposing (Sphere)
 
 
-{-| Navigator modes. There can be an orbiting mode, and an exploring mode.
+{-| Navigator modes. There can be an orbiting mode, and a surface mode.
 -}
 type Mode
     = Orbit Sphere
-    | Explore Sphere
+    | Surface Sphere
 
 
 type alias Navigator =
-    {}
+    { mode : Mode }
 
 
 {-| Initialize the navigator with its mode.
 -}
 init : Mode -> Navigator
 init mode =
-    {}
+    case mode of
+        Orbit sphere ->
+            initOrbit sphere
+
+        Surface sphere ->
+            initSurface sphere
+
+
+initOrbit : Sphere -> Navigator
+initOrbit sphere =
+    { mode = Orbit sphere }
+
+
+initSurface : Sphere -> Navigator
+initSurface sphere =
+    { mode = Surface sphere }
