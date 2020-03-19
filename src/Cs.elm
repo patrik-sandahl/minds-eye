4aa63abfa5d4cec6b79a3c074289607b06e5d55c
Patrik Sandahl <patrik.sandahl@gmail.com>
2020-03-19 20:55:02 +0100

    Removing unused stuff

--- src/Cs.elm
+++ src/Cs.elm
@@ -1,13 +1,9 @@
 module Cs exposing
-    ( rotateWorld
-    , toAxes
-    , worldAxes
-    , worldXAxis
+    ( worldXAxis
     , worldYAxis
     , worldZAxis
     )
 
-import Math.Matrix4 as M44 exposing (Mat4)
 import Math.Vector3 as V3 exposing (Vec3)
 
 
@@ -30,53 +26,3 @@ worldYAxis =
 worldZAxis : Vec3
 worldZAxis =
     V3.vec3 0.0 0.0 1.0
-
-
-{-| Make a matrix of the world axes.
--}
-worldAxes : Mat4
-worldAxes =
-    M44.makeBasis
-        worldXAxis
-        worldYAxis
-        worldZAxis
-
-
-{-| Decompose a matrix into x, y and z axes.
--}
-toAxes : Mat4 -> ( Vec3, Vec3, Vec3 )
-toAxes mat =
-    let
-        r =
-            M44.toRecord mat
-
-        x =
-            V3.vec3 r.m11 r.m21 r.m31
-
-        y =
-            V3.vec3 r.m12 r.m22 r.m32
-
-        z =
-            V3.vec3 r.m13 r.m23 r.m33
-    in
-    ( x, y, z )
-
-
-{-| Rotate the world matrix with the given azimuth and elevation radians.
--}
-rotateWorld : Float -> Float -> Mat4 -> Mat4
-rotateWorld azimuth elevation mat =
-    let
-        ( _, y, z ) =
-            toAxes mat
-
-        azimuthRot =
-            M44.makeRotate azimuth y
-
-        elevationRot =
-            M44.makeRotate elevation z
-
-        rot =
-            M44.mul azimuthRot elevationRot
-    in
-    M44.mul mat rot
