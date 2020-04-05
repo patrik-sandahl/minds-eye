70c6c6439ed7522fdede8f5b5b3932235d51a76e
Patrik Sandahl <patrik.sandahl@gmail.com>
2020-04-05 15:18:26 +0200

    Some stuff not yet working

--- src/Pipeline/Dev1FragmentShader.elm
+++ src/Pipeline/Dev1FragmentShader.elm
@@ -28,6 +28,29 @@ const float FarDistance = 100.0;
 const float SurfaceDistance = 0.001;
 const vec3 LightSource = vec3(10.0, 0.0, 0.0);
 
+const float terrainScale = 0.5;
+
+vec4 noised(vec3 x);
+
+mat3 makeTbn(vec3 pos)
+{
+    vec3 normal = normalize(pos - planetOrigo);
+    vec3 tangent = cross(vec3(0.0, 1.0, 0.0), normal);
+    vec3 bitangent = cross(normal, tangent);
+
+    return mat3(tangent, bitangent, normal);
+}
+
+vec3 normFromDeriv(vec3 deriv)
+{
+    //vec3 tangent = vec3(1.0, deriv.x, 0.0);
+    //vec3 bitangent = vec3(0.0, deriv.z, 1.0);
+
+    //return normalize(cross(bitangent, tangent));
+
+    return normalize(vec3(-deriv.x, 1.0, -deriv.z));
+}
+
 struct Ray {
     vec3 origin;
     vec3 direction;
@@ -63,9 +86,12 @@ float sphere(vec3 pos, float radius)
 
 vec4 intersectScene(vec3 p)
 {
-    float d = sphere(p - planetOrigo, planetRadius);
+    vec3 pos = p - planetOrigo;
+    vec4 n = noised(pos);
 
-    return vec4(d, 0.0, 0.0, 0.0);
+    n.x = sphere(p, planetRadius) + n.x * terrainScale;
+
+    return n;
 }
 
 vec4 rayMarch(Ray ray)
@@ -84,8 +110,6 @@ vec4 rayMarch(Ray ray)
     return v;
 }
 
-vec4 noised(vec3 x);
-
 void main()
 {
     vec2 uv = normalizedUV();
@@ -95,16 +119,28 @@ void main()
     vec4 v = rayMarch(ray);
     if (v.x < FarDistance) {
         vec3 iPoint = makePoint(ray, v.x);
-        //vec4 n = noised(iPoint);
-        vec3 normal = normalize(iPoint - planetOrigo);
-        //vec3 normal = normalize(n.yzw);
-        vec3 lightDir = normalize(LightSource - iPoint);
 
+        mat3 tbn = makeTbn(iPoint);
+        vec3 normal = normFromDeriv(v.yzw);
+
+
+        vec3 lightDir = normalize(LightSource - iPoint);
         float diffuse = max(0.0, dot(normal, lightDir));
+
+        color = vec3(diffuse);
+
+
+
+        //vec4 n = noised(iPoint);
+        //vec3 normal = normalize(iPoint - planetOrigo);
+        //vec3 normal = normalize(n.yzw);
+        //vec3 lightDir = normalize(LightSource - iPoint);
+
+        //float diffuse = max(0.0, dot(normal, lightDir));
 
         //color = vec3((n.x + 1.0) * 0.5);
         //color = normal;
-        color = vec3(diffuse);
+        //color = vec3(diffuse);
     }
 
     gl_FragColor = vec4(color, 1.0);
